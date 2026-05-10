3fd22f978406ed2fc416f5751d8fcdf5e82a9252
Eng_Yousef Elbana <zommzer@gmail.com>
2026-05-10 19:43:39 +0300

    Added Minimax algorithm implementation

---
+++ MinMax.py
@@ -0,0 +1,23 @@
+values =[[[5,6],[10,15]],[[20,30],[7,2]]]
+def minimax(node, ismax)
+
+    if type(node) == int
+        return node
+#max
+    if ismax
+        best = -float('inf')
+        for child in node
+            value = minimax(child, False)
+            best = max(best, value)
+        return best
+#min
+    else
+        best = float('inf')
+        for child in node
+            value = minimax(child, True)
+            best = min(best, value)
+        return best   
+
+
+print(minimax(values, True))
+     
